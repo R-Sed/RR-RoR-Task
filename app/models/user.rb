ac37583468cafa038d47f99a849de98ca0ed0754
R-Sed <ruslan.sedziukh@gmail.com>
2016-03-13 13:07:38 +0200

    Fixed bugs and add views for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  require 'devise/orm/active_record'
+  #config.orm = :active_record
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
