1d35ce6102965dfc8f6281b2ca3442f721bb3876
R-Sed <ruslan.sedziukh@gmail.com>
2016-03-25 22:56:28 +0200

    Add test for post#create

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,25 +30,14 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :view
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
 
-
-  before do
-
-   5.times do
-     user = create(:user)
-     5.times do
-       post = create(:post, user: user)
-       5.times do
-         comment = create(:comment, user: user, post: post)
-       end
-     end
-
-   end
- end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
